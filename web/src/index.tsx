c99568ead8d3b202ab6f04981e27b4c2d90c6d2f
Greg L <gregory.m.laurent@gmail.com>
2025-06-18 08:12:11 -0700

    animation example

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,20 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 import index from "../public/templates/index.html?raw";
 import test from "../public/templates/test.html?raw";
+import test2 from "../public/templates/test2.html?raw";
 
-app.get('/', (c) => {
+app.get("/", (c) => {
   return c.html(index as string);
-})
+});
 
-
-app.get('/test', (c) => {
+app.get("/test", (c) => {
   return c.html(test as string);
-})
+});
 
-export default app
+app.get("/test2", (c) => {
+  return c.html(test2 as string);
+});
+
+export default app;
